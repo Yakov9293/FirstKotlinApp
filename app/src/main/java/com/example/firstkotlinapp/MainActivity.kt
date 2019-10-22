98ba7c36084dcfe31216cbf1988b3bdfc81ba200
Yakov9293 <yakov226805@yandex.ru>
2019-10-22 20:27:58 +0300

    добавил один комментарий с вопросом

--- app/src/main/java/com/example/firstkotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/firstkotlinapp/MainActivity.kt
@@ -4,12 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
 
         val addTask: View = findViewById(R.id.addTask)//адекватно ли определять кнопку здесь, можно и нужно ли выносить её и её определение в отдельный класс
         addTask.setOnClickListener { view ->
